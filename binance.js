35accea0d0f7f39f28b15a80049616462ee48811
Xavi Carceller <elxavicio@gmail.com>
2018-01-05 11:15:59 +0100

    Added get deposit addresses request

--- binance.js
+++ binance.js
@@ -1,30 +1,35 @@
-const qs = require('querystring')
-const crypto = require('crypto')
+const qs = require("querystring");
+const crypto = require("crypto");
 
 const Binance = {
-  init: function(config) {  
-    this.v1URL = 'https://www.binance.com/api/v1/'
-    this.v3URL = 'https://www.binance.com/api/v3/'
-    this.secretKey = config.secretKey
+  init: function(config) {
+    this.v1URL = "https://www.binance.com/api/v1/";
+    this.v3URL = "https://www.binance.com/api/v3/";
+    this.v3WAPIURL = "https://www.binance.com/wapi/v3/";
+    this.secretKey = config.secretKey;
     this.headers = {
       headers: {
-        'X-MBX-APIKEY': config.apiKey,
-        'Content-Type': 'application/x-www-form-urlencoded'
+        "X-MBX-APIKEY": config.apiKey,
+        "Content-Type": "application/x-www-form-urlencoded"
       }
-    }
-    const Public = require('./lib/public')
-    const Account = require('./lib/account')
-    const Sockets = require('./lib/websockets')
-    Object.assign(this, Public, Account, Sockets)
+    };
+    const Public = require("./lib/public");
+    const Account = require("./lib/account");
+    const Sockets = require("./lib/websockets");
+    Object.assign(this, Public, Account, Sockets);
   },
   sign: function(queryString) {
-    return '&signature=' + crypto.createHmac('sha256', this.secretKey)
-      .update(queryString)
-      .digest('hex')
+    return (
+      "&signature=" +
+      crypto
+        .createHmac("sha256", this.secretKey)
+        .update(queryString)
+        .digest("hex")
+    );
   },
   formatQuery: function(queryString) {
-    return '?' + qs.stringify(queryString)
+    return "?" + qs.stringify(queryString);
   }
-}
+};
 
-module.exports = Binance
+module.exports = Binance;
